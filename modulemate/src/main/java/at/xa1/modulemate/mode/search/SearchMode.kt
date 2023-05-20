006f26ac97bf1542c29420d590f92c990c073cbd
Daniel Gehrer <daniel.dev@xa1.at>
2023-05-20 16:00:58 +0200

    Refactor SearchMode to Screen

--- modulemate/src/main/java/at/xa1/modulemate/mode/search/SearchMode.kt
+++ modulemate/src/main/java/at/xa1/modulemate/mode/search/SearchMode.kt
@@ -2,39 +2,58 @@ package at.xa1.modulemate.mode.search
 
 import at.xa1.modulemate.mode.LiveUiMode
 import at.xa1.modulemate.mode.ModulesListItemRenderer
+import at.xa1.modulemate.mode.SearchListScreen
 import at.xa1.modulemate.module.Modules
-import at.xa1.modulemate.ui.ListBox
-import at.xa1.modulemate.ui.TextBox
 import at.xa1.modulemate.ui.Ui
 import at.xa1.modulemate.ui.UiUserInput
 
-class SearchMode(
+internal class SearchMode(
     private val ui: Ui,
     private val modules: Modules
 ) : LiveUiMode {
-    private var state = SearchScreenState(
-        searchBox = TextBox(
-            hint = "Search Mode: Type to search modules and commands",
-            emoji = "🔍"
-        ),
-        listBox = ListBox(
-            items = modules.allModules,
-            height = 0,
-            itemRenderer = ModulesListItemRenderer(modules)
-        )
+    private val screen = SearchListScreen(
+        emoji = "🔍",
+        hint = "Search Mode: Type to search modules",
+        listProvider = { filter ->
+            val filterTokens = filter.split(' ')
+            modules.allModules.filter { module ->
+                filterTokens.all { token -> module.path.contains(token) }
+            }
+        },
+        listItemRenderer = ModulesListItemRenderer(modules)
     )
 
     override fun print(input: UiUserInput?) {
-        val context = ui.createScreenContext()
+        while (true) {
+            screen.print(ui)
 
-        state = if (input != null) {
-            state.reduce(input, modules, context.size.rows)
-        } else {
-            state.updateHeight(context.size.rows)
+            when (val input = ui.readUserInput()) {
+                UiUserInput.Tab, UiUserInput.Shift.Tab -> return
+                UiUserInput.Return -> selectModule()
+                else -> {
+                    screen.input(input)
+                }
+            }
         }
+    }
 
-        context.printScreen {
-            searchScreen(this, state)
+    private fun selectModule() {
+        val selectedModule = screen.selectedItem
+        val allItemsInList = screen.state.listBox.items
+
+        if (selectedModule != null) {
+            if (modules.isActive(selectedModule)) {
+                modules.removeRecent(selectedModule)
+            } else {
+                modules.activate(selectedModule)
+            }
+        } else {
+            val allActive = allItemsInList.all { module -> modules.isActive(module) }
+            if (allActive) {
+                allItemsInList.forEach { module -> modules.removeRecent(module) }
+            } else {
+                allItemsInList.forEach { module -> modules.activate(module) }
+            }
         }
     }
 }
